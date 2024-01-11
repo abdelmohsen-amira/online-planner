b1e9b7934a35f8fa853d59e57eef8de0a8c812e7
abdelmohsen-amira <147008423+abdelmohsen-amira@users.noreply.github.com>
2024-01-11 15:24:07 -0500

    Add files via upload

---
+++ breathingComponent.js
@@ -0,0 +1,44 @@
+// BreathingComponent.js
+import React, { useState, useEffect } from 'react';
+import './breathingComponent.css';
+
+const items = ['breathe in for 7 seconds', 'breathe out for 11 seconds', 'repeat for 12 to 15 repetitions', 'Done!'];
+
+const BreathingComponent = () => {
+  const [visibleItems, setVisibleItems] = useState([]);
+  const [animationStarted, setAnimationStarted] = useState(false);
+
+  useEffect(() => {
+    if (animationStarted) {
+      const interval = setInterval(() => {
+        if (items.length > visibleItems.length) {
+          setVisibleItems((prevVisibleItems) => [
+            ...prevVisibleItems,
+            items[prevVisibleItems.length],
+          ]);
+        } else {
+          clearInterval(interval);
+        }
+      }, 1000);
+
+      return () => clearInterval(interval);
+    }
+  }, [animationStarted, visibleItems]);
+
+  const startAnimation = () => {
+    setAnimationStarted(true);
+  };
+
+  return (
+    <div>
+      <ul className="list">
+        {visibleItems.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+      <button onClick={startAnimation}>click to start exercise</button>
+    </div>
+  );
+};
+
+export default BreathingComponent;
